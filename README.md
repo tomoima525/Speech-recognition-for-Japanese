25b728edda17cf6ead78296f3f74a9d386b4099c
Tomoaki Imai <tomoima525@gmail.com>
2020-11-28 09:59:35 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Speech Recognition for Japanese audio
+
+- Specified for Japanese audio
+
+# How to use
+
+### Initialization
+
+- TBD 
+
+### Audio setup
+
+- TBD
+
+### Google Storage setup (grant permission)
+
+- TBD
---
+++ src/index.ts
@@ -0,0 +1,77 @@
+import speech, { protos } from '@google-cloud/speech';
+
+const client = new speech.SpeechClient();
+const config = {
+  audioChannelCount: 2,
+  enableWordTimeOffsets: true,
+  enableAutomaticPunctuation: true,
+  // Speaker diarization
+  enableSpeakerDiarization: true,
+  diarizationSpeakerCount: 3,
+  encoding: protos.google.cloud.speech.v1.RecognitionConfig.AudioEncoding.FLAC,
+  sampleRateHertz: 44100,
+  languageCode: 'ja-JP'
+};
+
+const audio = {
+  uri: 'gs://cloud-samples-tests/speech/brooklyn.flac'
+};
+
+const request = {
+  config,
+  audio
+};
+
+const wait = (milisec = 1000) => new Promise((resolve) => setTimeout(() => resolve(), milisec));
+
+async function transcribeAudio() {
+  try {
+    const [operation] = await client.longRunningRecognize(request);
+    const name = operation?.name;
+    if (!name) throw new Error('name is not defined');
+
+    let running = true;
+
+    while (running) {
+      const decodedOperation = await client.checkLongRunningRecognizeProgress(name);
+      // We know for sure metadata is LongRunningRecognizeMetadata
+      const meta = (decodedOperation?.metadata as unknown) as protos.google.cloud.speech.v1.LongRunningRecognizeMetadata;
+      const percent = meta?.progressPercent || 0;
+      console.log(`progress ${percent}% ${JSON.stringify(decodedOperation)}`);
+      running = !(decodedOperation.done || false);
+      if (percent === 100) {
+        console.log(`Done! startTime: ${meta.startTime?.seconds} endTime: ${meta.lastUpdateTime?.seconds}\n`);
+      } else {
+        await wait(10000);
+      }
+    }
+
+    console.log('\nResult:');
+    const [response] = await operation.promise();
+    response.results?.forEach((result) => {
+      const alternatives = result?.alternatives || [];
+      if (alternatives.length <= 0) {
+        console.log('No transcribe\n');
+        return;
+      }
+      const wordsInfo = alternatives[0].words || [];
+      if (wordsInfo.length <= 0) {
+        console.log(`(unknown): ${alternatives[0].transcript}\n`);
+        return;
+      }
+      const speakerTag = wordsInfo[0]?.speakerTag || '-';
+      console.log(`(${speakerTag}): ${alternatives[0].transcript}\n`);
+      // TODO: log every 60 second
+      // alternatives[0].words?.forEach((wordInfo) => {
+      //   const startSecs = `${wordInfo?.startTime?.seconds}` + '.' + wordInfo?.startTime?.nanos || 0 / 100000000;
+      //   const endSecs = `${wordInfo?.endTime?.seconds}` + '.' + wordInfo?.endTime?.nanos || 0 / 100000000;
+      //   console.log(`Word: ${wordInfo.word}`);
+      //   console.log(`\t ${startSecs} secs - ${endSecs} secs`);
+      // });
+    });
+  } catch (e) {
+    console.log({ error: e });
+  }
+}
+
+transcribeAudio();
