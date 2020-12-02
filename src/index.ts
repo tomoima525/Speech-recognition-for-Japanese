3a09648cbd65ce048bb630612d7114a5ba854874
Tomoaki Imai <tomoima525@gmail.com>
2020-12-01 21:42:27 -0800

    dialization

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import fs from 'fs';
 import speech, { protos } from '@google-cloud/speech';
 import addPunctuation from './addPunctuation';
 
-const client = new speech.SpeechClient();
+const client = new speech.v1p1beta1.SpeechClient();
 const config = {
   audioChannelCount: 2,
   enableWordTimeOffsets: true,
@@ -26,6 +27,10 @@ const request = {
 const wait = (milisec = 1000) => new Promise((resolve) => setTimeout(() => resolve(), milisec));
 
 async function transcribeAudio() {
+  if (fs.existsSync('result.txt')) {
+    fs.unlinkSync('result.txt');
+  }
+  fs.openSync('result.txt', 'w');
   try {
     const [operation] = await client.longRunningRecognize(request);
     const name = operation?.name;
@@ -41,7 +46,9 @@ async function transcribeAudio() {
       console.log(`progress ${percent}%`);
       running = !(decodedOperation.done || false);
       if (percent === 100) {
-        console.log(`Done! startTime: ${meta.startTime?.seconds} endTime: ${meta.lastUpdateTime?.seconds}\n`);
+        const endTime: number = (meta.lastUpdateTime?.seconds as number) || 0;
+        const startTime: number = (meta.startTime?.seconds as number) || 0;
+        console.log(`Done! time: ${endTime - startTime}\n`);
       } else {
         await wait(10000);
       }
@@ -49,29 +56,48 @@ async function transcribeAudio() {
 
     console.log('\nResult:');
     const [response] = await operation.promise();
-    response.results?.forEach((result) => {
-      const alternatives = result?.alternatives || [];
-      if (alternatives.length <= 0) {
-        console.log('No transcribe\n');
-        return;
+
+    if (!response.results || response.results?.length === 0) {
+      console.log('No result');
+      return;
+    }
+
+    const result = response.results[response.results.length - 1];
+    const alternatives = result?.alternatives || [];
+    if (alternatives.length <= 0) {
+      console.log('No transcribe\n');
+      return;
+    }
+
+    // const rawdata = fs.readFileSync('test-data.json');
+    // const wordsInfo = JSON.parse(rawdata.toString('utf8'));
+
+    const wordsInfo = alternatives[0].words || [];
+    const speakArr: protos.google.cloud.speech.v1p1beta1.IWordInfo[] = [];
+    for (const wordInfo of wordsInfo) {
+      if (speakArr.length === 0) {
+        speakArr.push(wordInfo);
+        continue;
       }
-      const wordsInfo = alternatives[0].words || [];
-      if (wordsInfo.length <= 0) {
-        console.log(`(unknown): ${alternatives[0].transcript}\n`);
-        return;
+      if (wordInfo.speakerTag === speakArr[0].speakerTag) {
+        speakArr.push(wordInfo);
+        continue;
       }
-      const speakerTag = wordsInfo[0]?.speakerTag || '-';
-      console.log(`(${speakerTag}): ${alternatives[0].transcript}\n`);
-      const words = alternatives[0].words?.map((wordsInfo) => wordsInfo?.word || '').filter((w) => w !== '');
-      console.log(`(punctuated):${addPunctuation(words)}`);
-      // TODO: log every 60 second
-      // alternatives[0].words?.forEach((wordInfo) => {
-      //   const startSecs = `${wordInfo?.startTime?.seconds}` + '.' + wordInfo?.startTime?.nanos || 0 / 100000000;
-      //   const endSecs = `${wordInfo?.endTime?.seconds}` + '.' + wordInfo?.endTime?.nanos || 0 / 100000000;
-      //   console.log(`Word: ${wordInfo.word}`);
-      //   console.log(`\t ${startSecs} secs - ${endSecs} secs`);
-      // });
-    });
+      const words = speakArr.map((wordsInfo) => wordsInfo?.word || '').filter((w) => w !== '');
+      //console.log(`(p-${speakArr[0].speakerTag}):${addPunctuation(words)}`);
+      fs.appendFileSync('result.txt', `(p-${speakArr[0].speakerTag}):${addPunctuation(words)}\n`);
+      speakArr.splice(0, speakArr.length);
+      speakArr.push(wordInfo);
+    }
+
+    console.log('Done!');
+    // TODO: log every 60 second
+    // alternatives[0].words?.forEach((wordInfo) => {
+    //   const startSecs = `${wordInfo?.startTime?.seconds}` + '.' + wordInfo?.startTime?.nanos || 0 / 100000000;
+    //   const endSecs = `${wordInfo?.endTime?.seconds}` + '.' + wordInfo?.endTime?.nanos || 0 / 100000000;
+    //   console.log(`Word: ${wordInfo.word}`);
+    //   console.log(`\t ${startSecs} secs - ${endSecs} secs`);
+    // });
   } catch (e) {
     console.log({ error: e });
   }
